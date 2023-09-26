0bd396e611d353a27152d0e750f0d6b6dd3df024
izemaghilas <saidtala23@gmail.com>
2023-09-26 21:46:35 +0200

    fix colors

--- front/src/pages/dashboards/SaaS.tsx
+++ front/src/pages/dashboards/SaaS.tsx
@@ -1,91 +1,85 @@
-import { Box, Grid, useTheme } from "@mui/material";
-import Analytics from "../../components/Dashboards/saas/Analytics";
-import SaaSCard from "../../components/Dashboards/saas/Card";
-import Footer from "../../components/Dashboards/saas/Footer";
-import RecentOrders from "../../components/Dashboards/saas/RecentOrders";
-import TopSelling from "../../components/Dashboards/saas/TopSelling";
-import TotalSpent from "../../components/Dashboards/saas/TotalSpent";
+import { useTheme } from "@mui/material";
+import { FC } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import { ROLES } from "../../constants";
+import useAuth from "../../hooks/useAuth";
 import useTitle from "../../hooks/useTitle";
 import BucketIcon from "../../icons/BucketIcon";
 import EarningIcon from "../../icons/EarningIcon";
 import PeopleIcon from "../../icons/PeopleIcon";
 import WindowsLogoIcon from "../../icons/WindowsLogoIcon";
-import { FC } from "react";
-import useAuth from "../../hooks/useAuth";
-import { ROLES } from "../../constants";
-import { Navigate, useNavigate } from "react-router-dom";
 
 const SaaS: FC = () => {
-  // change navbar title
-  useTitle("Saas");
-  const {user} = useAuth()
-  const theme = useTheme();
-  const navigate = useNavigate()
+    // change navbar title
+    useTitle("Saas");
+    const { user } = useAuth();
+    const theme = useTheme();
+    const navigate = useNavigate();
 
-  const cardList = [
-    {
-      price: 574,
-      Icon: BucketIcon,
-      title: "All Spending",
-      color: theme.palette.primary.main,
-    },
-    {
-      price: 521,
-      title: "Earnings",
-      Icon: EarningIcon,
-      color: theme.palette.primary.purple,
-    },
-    {
-      price: 684,
-      Icon: WindowsLogoIcon,
-      title: "Weekly revenue",
-      color: theme.palette.primary.red,
-    },
-    {
-      price: 321,
-      Icon: PeopleIcon,
-      title: "New Clients",
-      color: theme.palette.primary.yellow,
-    },
-  ];
+    const cardList = [
+        {
+            price: 574,
+            Icon: BucketIcon,
+            title: "All Spending",
+            color: theme.palette.primary.main,
+        },
+        {
+            price: 521,
+            title: "Earnings",
+            Icon: EarningIcon,
+            color: theme.palette.primary.purple,
+        },
+        {
+            price: 684,
+            Icon: WindowsLogoIcon,
+            title: "Weekly revenue",
+            color: theme.palette.primary.red,
+        },
+        {
+            price: 321,
+            Icon: PeopleIcon,
+            title: "New Clients",
+            color: theme.palette.primary.yellow,
+        },
+    ];
 
-  if(user?.role === ROLES.admin) {
-    return <Navigate to="user-list" />
-  }
+    if (user?.role === ROLES.admin) {
+        return <Navigate to="user-list" />;
+    }
 
-  // TODO: navigate to events, if role user
+    return <Navigate to="training-list" />;
 
-  return (
-    <Box pt={2} pb={4}>
-      <Grid container spacing={{ xs: 2, sm: 3, md: 4 }}>
-        {cardList.map((card, index) => (
-          <Grid item lg={3} xs={6} key={index}>
-            <SaaSCard card={card} />
-          </Grid>
-        ))}
-      </Grid>
+    // return (
+    //   <Box pt={2} pb={4}>
+    //     <Grid container spacing={{ xs: 2, sm: 3, md: 4 }}>
+    //       {cardList.map((card, index) => (
+    //         <Grid item lg={3} xs={6} key={index}>
+    //           <SaaSCard card={card} />
+    //         </Grid>
+    //       ))}
+    //     </Grid>
 
-      <Grid container spacing={4} pt={4}>
-        <Grid item lg={8} md={7} xs={12}>
-          <TotalSpent />
-        </Grid>
-        <Grid item lg={4} md={5} xs={12}>
-          <Analytics />
-        </Grid>
+    //     <Grid container spacing={4} pt={4}>
+    //       <Grid item lg={8} md={7} xs={12}>
+    //         <TotalSpent />
+    //       </Grid>
+    //       <Grid item lg={4} md={5} xs={12}>
+    //         <Analytics />
+    //       </Grid>
 
-        <Grid item lg={8} md={7} xs={12}>
-          <RecentOrders />
-        </Grid>
-        <Grid item lg={4} md={5} xs={12}>
-          <TopSelling />
-        </Grid>
+    //       <Grid item lg={8} md={7} xs={12}>
+    //         <RecentOrders />
+    //       </Grid>
+    //       <Grid item lg={4} md={5} xs={12}>
+    //         <TopSelling />
+    //       </Grid>
 
-        <Grid item xs={12}>
-          <Footer imageLink="/static/illustration/sass-dashboard.svg" />
-        </Grid>
-      </Grid>
-    </Box>
-  );
+    //       <Grid item xs={12}>
+    //         <Footer imageLink="/static/illustration/sass-dashboard.svg" />
+    //       </Grid>
+    //     </Grid>
+    //   </Box>
+    // );
 };
 
 export default SaaS;
